26312e8fd476fc57dde7000db94c85cba7df8203
Sae <winny9641@gmail.com>
2022-05-22 08:08:03 -0400

    object properties

---
+++ object-properties.js
@@ -0,0 +1,12 @@
+const example = {  
+  pizza: 'yummy'  
+}  
+  
+//console.log(example['pizza']) ;
+//console.log(example.pizza);
+
+const food = {  
+  types: 'only pizza'  
+}  
+
+console.log(food.types);
